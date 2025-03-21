ce85d9956263695b61b5352988c62decd73b22ef
BrunoSpoletini <bruno_spoletini@hotmail.com>
2025-03-21 11:29:54 -0300

    Filtros y Csv

--- runner.py
+++ runner.py
@@ -1,34 +1,64 @@
 import subprocess
 import os
+import csv
 
-compiladores = ["g++", "clang"]
+compiladores = ["g++"] #["g++", "clang"]
 optimizadores = ["-O0"]
 #optimizadores = ["-O0", "-O1", "-O2", "-O3", "-Ofast"]
-landscapes = [("./data/2000_8", 16)]
+landscapes = [("./data/2000_8", 16)] # 16
 #landscapes = [("./data/2000_8", 16), ("./data/1999_27j_S", 16), ("./data/2015_50", 4)]
+
+# Filtra el tiempo de usuario promedio de todas las ejecuciones
+def filtrarPerf(res):
+    tiempo_promedio = None
+    for linea in res.stderr.splitlines():
+        if "seconds time elapsed" in linea:
+            partes = linea.strip().split()
+            tiempo_promedio = partes[0]
+            break
+    return tiempo_promedio
+
+# Promedia la cantidad de celdas quemadas por segundo de varias ejecuciones
+def mediaCeldasPorSeg(res):
+    lines = res.stderr.splitlines()
+    sumaCeldas = 0
+    for line in lines:
+        partes = line.strip().split()
+        num = partes[-1]
+        sumaCeldas += float(num)
+    return sumaCeldas/len(lines)
 
 def main():
 
     resultados = dict()
 
-    for landscape in landscapes:
-        for comp in compiladores:
-            resultados[comp] = []
-            for opt in optimizadores:
-                os.system("make clean && make CXX=" + comp + " CXXOPT=" + opt)
+    # comando = ["/usr/lib/linux-tools/5.15.0-134-generic/perf", "stat", "-r", "1", "./graphics/fire_animation_data" , "./data/2000_8", "0"]
+    # res = subprocess.run(comando, stderr=subprocess.PIPE, text=True)
+    # tiempo_promedio_user = filtrarPerf(res)
+    # print("Tiempo medio de ejecución:", tiempo_promedio_user, "segundos")
+    # # Cuando usariamos esto? Si la idea es usar la metrica.
 
-                comando = ["perf", "stat", "-r", str(landscape[1]), "./grafics/fire_animation_data" , landscape[0], "0"]
-                resultado = subprocess.run(comando, stderr=subprocess.PIPE, text=True)
 
-                print(resultado)
-    #             for linea in resultado.stderr.split("\n"):
-    #                 if "time elapsed" in linea:
-    #                     resultados[comp].insert(re.findall(r"[\d.]+", linea)[0])
+    # comando = ["/usr/lib/linux-tools/5.15.0-134-generic/perf", "stat", "-r", "5", "--quiet", "./graphics/fire_animation_data" , "./data/2000_8", "0"]
+    # res = subprocess.run(comando, stderr=subprocess.PIPE, text=True)
+    # print("Tiempo medio de celdas por segundo: ", mediaCeldasPorSeg(res))
 
-    #             print(resultados)
+    with open('resultados.csv', mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Compilador", "Optimizador", "Landscape", "Tiempo promedio de ejecuciones", "Celdas quemadas por segundo"])
+        for landscape in landscapes:
+            for comp in compiladores:
 
-    # print(resultados)
+                for opt in optimizadores:
+                    os.system("make clean && make CXX=" + comp + " CXXOPT=" + opt)
+                    comandoPerf = ["/usr/lib/linux-tools/5.15.0-134-generic/perf", "stat", "-r", str(landscape[1]), "./graphics/fire_animation_data" , landscape[0], "0"]
+                    resPerf = subprocess.run(comandoPerf, stderr=subprocess.PIPE, text=True)
 
+                    comandoCells = ["/usr/lib/linux-tools/5.15.0-134-generic/perf", "stat", "-r", str(landscape[1]), "--quiet", "./graphics/fire_animation_data" , landscape[0], "0"]
+                    resCells = subprocess.run(comandoCells, stderr=subprocess.PIPE, text=True)
+
+                    # exportar datos a csv:
+                    writer.writerow([comp, opt, landscape[0], filtrarPerf(resPerf), mediaCeldasPorSeg(resCells)])
 
 if __name__ == "__main__":
     main()
